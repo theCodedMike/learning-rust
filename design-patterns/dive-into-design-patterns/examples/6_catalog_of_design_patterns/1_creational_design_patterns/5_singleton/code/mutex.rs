da835e631bdd0c60790ca13f7d9bd21e3018c533
mike <lixiaofeng1115@outlook.com>
2023-07-11 02:57:18 +0800

    补充单例模式示例

--- design-patterns/dive-into-design-patterns/examples/6_catalog_of_design_patterns/1_creational_design_patterns/5_singleton/code/mutex.rs
+++ design-patterns/dive-into-design-patterns/examples/6_catalog_of_design_patterns/1_creational_design_patterns/5_singleton/code/mutex.rs
@@ -8,12 +8,32 @@
 //! Now that `Mutex::new` is `const`, you can use global static `Mutex` locks
 //! without needing lazy initialization.
 
-use std::sync::Mutex;
+use std::sync::{Mutex, MutexGuard, PoisonError};
 
 static ARRAY: Mutex<Vec<i32>> = Mutex::new(Vec::new());
 
+static USER_CONTROLLER: Mutex<UserController> = Mutex::new(UserController { count: 0 });
+
 fn do_a_call() {
     ARRAY.lock().unwrap().push(1);
+}
+
+struct UserController {
+    count: usize,
+}
+impl UserController {
+    fn get_singleton() -> Result<MutexGuard<'static, Self>, PoisonError<MutexGuard<'static, Self>>>
+    {
+        USER_CONTROLLER.lock()
+    }
+
+    fn add_one(&mut self) {
+        self.count += 1;
+    }
+
+    fn print(&self, thread_id: usize) {
+        println!("ThreadId: {:4}, now count is {}", thread_id, self.count);
+    }
 }
 
 /// cargo r --example singleton-mutex
@@ -23,4 +43,24 @@ fn main() {
     do_a_call();
 
     println!("Called {} times", ARRAY.lock().unwrap().len());
+
+    let mut handlers = vec![];
+
+    for i in 1..=1000 {
+        let handle = std::thread::spawn(move || match UserController::get_singleton() {
+            Ok(mut lock) => {
+                lock.add_one();
+                lock.print(i);
+            }
+            Err(_) => {
+                println!("ThreadId: {:4}, failed to get lock", i);
+            }
+        });
+        handlers.push(handle);
+    }
+
+    handlers.into_iter().for_each(|h| h.join().unwrap());
+
+    UserController::get_singleton().unwrap().add_one();
+    UserController::get_singleton().unwrap().print(0);
 }
--- design-patterns/dive-into-design-patterns/examples/6_catalog_of_design_patterns/1_creational_design_patterns/5_singleton/code/once_lock.rs
+++ design-patterns/dive-into-design-patterns/examples/6_catalog_of_design_patterns/1_creational_design_patterns/5_singleton/code/once_lock.rs
@@ -1,23 +1,65 @@
+#![allow(dead_code)]
 // use std::cell::OnceCell;
 use std::sync::OnceLock; // This type is a thread-safe OnceCell
 
-static USER_CONTROLLER: OnceLock<UserController> = OnceLock::new();
+static mut USER_CONTROLLER: OnceLock<UserController> = OnceLock::new();
+static CELL: OnceLock<OrderService> = OnceLock::new();
 
-struct UserController;
+struct OrderService;
 
-impl UserController {
+impl OrderService {
     fn get_singleton() -> &'static Self {
-        USER_CONTROLLER.get_or_init(|| UserController)
+        CELL.get_or_init(|| OrderService)
     }
 
     fn name(&self) {
-        println!("this is UserController");
+        println!("This is OrderService");
+    }
+}
+
+#[derive(Debug)]
+struct UserController {
+    count: usize,
+}
+
+impl UserController {
+    fn get_singleton() -> &'static mut Self {
+        unsafe {
+            // 这里无法锁住，失败
+            if USER_CONTROLLER.get_mut().is_none() {
+                USER_CONTROLLER
+                    .set(UserController { count: 0 })
+                    .expect("Failed to set");
+            }
+            USER_CONTROLLER.get_mut().unwrap()
+        }
+    }
+
+    fn print(&self, thread_id: usize) {
+        println!("ThreadId: {:4}, now count is {}", thread_id, self.count);
+    }
+
+    fn add_one(&mut self) {
+        self.count += 1;
     }
 }
 
 /// cargo r --example singleton-once-lock
 fn main() {
-    let singleton = UserController::get_singleton();
+    // 无法锁住，失败
+    /*let mut handlers = vec![];
 
-    singleton.name();
+    (1..=1000).for_each(|i| {
+        let handle = std::thread::spawn(move || {
+            let user = UserController::get_singleton();
+            user.add_one();
+            user.print(i);
+        });
+        handlers.push(handle);
+    });
+
+    handlers.into_iter().for_each(|h| h.join().unwrap());*/
+
+    let order = OrderService::get_singleton();
+    order.name();
 }
